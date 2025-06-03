17d3d3b3d8b436d5e7fbc29c765ced754d038961
YCY <chengyanyang0510@gmail.com>
2025-06-03 22:38:23 +0800

    Initial Commit

---
+++ DDayCountdown/DDayCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDayCountdownApp.swift
+//  DDayCountdown
+//
+//  Created by Cheng Yan Yang on 2025/6/3.
+//
+
+import SwiftUI
+
+@main
+struct DDayCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
